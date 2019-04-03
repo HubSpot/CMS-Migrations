0f93900e26c1eb975e1190d11c3db1706cd77f27
Jason Rosa <jasonrosa838@gmail.com>
2019-04-03 15:09:55 -0400

    Updating JS to use debounce
    Updating JS to use debounce per suggestion in #migration-resources Slack channel for performance improvements on the scroll function. More information on debounce can be found here (https://davidwalsh.name/javascript-debounce-function).

--- Modules/backToTop/module.js
+++ Modules/backToTop/module.js
@@ -1,16 +1,33 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("toTopButton").style.display = "block";
-  } else {
-    document.getElementById("toTopButton").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
+// Declarations
 var toTopButton = document.getElementById('toTopButton');
+
+// Functions  
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+  var context = this, args = arguments;
+  var later = function() {
+    timeout = null;
+    if (!immediate) func.apply(context, args);
+  };
+  var callNow = immediate && !timeout;
+  clearTimeout(timeout);
+  timeout = setTimeout(later, wait);
+  if (callNow) func.apply(context, args);
+  };
+};
+
+var scrollFunction = debounce(function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
+}, 30);
+
+// Event Listeners 
+window.addEventListener('scroll', debounce(scrollFunction));
+
 toTopButton.addEventListener("click", function(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
